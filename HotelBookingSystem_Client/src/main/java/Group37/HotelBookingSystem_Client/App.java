9d5cb5664e7a82b377610336f22e4d98de70eba9
Lenovo-PC\Lenovo <151250054@smail.nju.edu.cn>
2016-10-14 22:27:12 +0800

    新建两个maven project

---
+++ HotelBookingSystem_Client/src/main/java/Group37/HotelBookingSystem_Client/App.java
@@ -0,0 +1,13 @@
+package Group37.HotelBookingSystem_Client;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ HotelBookingSystem_Server/src/main/java/Group37/HotelBookingSystem/App.java
@@ -0,0 +1,13 @@
+package Group37.HotelBookingSystem;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
