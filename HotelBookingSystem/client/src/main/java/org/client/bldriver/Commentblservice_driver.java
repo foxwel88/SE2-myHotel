d870a4e80eab1ef5dd0b5bf817e5e94a5cda9579
fraliphsoft <151250054@smail.nju.edu.cn>
2016-10-15 19:51:35 +0800

    finish bl-level driver

--- HotelBookingSystem/client/src/main/java/org/client/bldriver/Commentblservice_driver.java
+++ HotelBookingSystem/client/src/main/java/org/client/bldriver/Commentblservice_driver.java
@@ -1,5 +1,18 @@
 package org.client.bldriver;
 
-public class Commentblservice_driver {
+import java.util.ArrayList;
+import java.util.Date;
 
+import org.client.blstub.Comment_stub;
+import org.client.vo.CommentVO;
+
+public class Commentblservice_driver {
+	
+	public static void main(String[] args){
+		Comment_stub stub = new Comment_stub();
+		
+		System.out.println(stub.getComment("GodHotel"));
+		
+		System.out.println(stub.addComment(new CommentVO("me", "you", new Date(), 110, "Too Bad!")));		
+	}
 }
