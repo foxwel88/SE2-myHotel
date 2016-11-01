fb311832c236d7dc21de94cf3cb74f75a72bd608
HikariChao <gz120748@gmail.com>
2016-11-01 21:15:40 +0800

    type
    rt

--- HotelBookingSystem/common/src/main/java/org/common/utility/OrderType.java
+++ HotelBookingSystem/common/src/main/java/org/common/utility/OrderType.java
@@ -1,5 +1,22 @@
 package org.common.utility;
 
 public enum OrderType {
-	UNEXECUTED, EXECUTED, CANCELED, ABNORMAL
+
+	UNEXECUTED("1"), EXECUTED("2"), CANCELED("3"), ABNORMAL("异常订单");
+	private String s;
+	private OrderType(String ss) {
+		this.s = ss;
+	}
+	
+	public String getstring()
+	{
+		return s;
+	}
+	
+	public OrderType gettype(String ss)
+	{
+		if(ss.equals("异常订单"))
+		return OrderType.ABNORMAL;
+		return null;
+	}
 }
