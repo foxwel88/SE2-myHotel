67cb323e3592e3313e6a2c452aac1fcbc52d77e2
fraliphsoft <151250054@smail.nju.edu.cn>
2016-11-03 19:19:26 +0800

    建立了comment和promotion模块在bl层的具体类，修改了详细设计文档

--- HotelBookingSystem/client/src/main/java/org/client/bl/promotionbl/Promotion.java
+++ HotelBookingSystem/client/src/main/java/org/client/bl/promotionbl/Promotion.java
@@ -1,47 +1,12 @@
 package org.client.bl.promotionbl;
 
-import java.util.List;
-
-import org.client.blservice.promotionblservice.Promotionblservice;
-import org.client.vo.LevelVO;
 import org.client.vo.PromotionVO;
 import org.common.utility.ResultMessage;
 
-public class Promotion implements Promotionblservice{
+public class Promotion {
 
-	public ResultMessage add(PromotionVO vo) {
-		// TODO Auto-generated method stub
+	public ResultMessage modify (PromotionVO vo){
 		return null;
 	}
-
-	public List<PromotionVO> getPromotion(String hotelAddress, String userID) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	public List<PromotionVO> showHotelPromotion(String hotelAddress) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	public List<PromotionVO> showWebsitePromotion() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	public ResultMessage modify(PromotionVO vo) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	public LevelVO showLevel() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	public ResultMessage modifyLevel(LevelVO vo) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
+	
 }
