751b963cd275d2e8cc680842647b4202598f991a
fraliphsoft <151250054@smail.nju.edu.cn>
2016-11-21 16:38:32 +0800

    把昨天没写完的导航栏跳转写完了，先同步一个不报错的版本上来

--- HotelBookingSystem/client/src/main/java/org/client/presentation/customer/CustomerController_CusInfo.java
+++ HotelBookingSystem/client/src/main/java/org/client/presentation/customer/CustomerController_CusInfo.java
@@ -6,33 +6,33 @@ import javafx.scene.layout.GridPane;
 
 public class CustomerController_CusInfo {
 	@FXML
-	private GridPane gridpane;
+	public GridPane gridpane;
 	
 	@FXML
 	private AnchorPane root;
 	
 	@FXML
 	void turnToCusController_Main() {
-		SwitchSceneUtil.turnToCusController_Main(gridpane);
+		SwitchSceneUtil.turnToCusController_Main();
 	}
 	
 	@FXML
 	void turnToCusController_CusInfo() {
-		SwitchSceneUtil.turnToCusController_CusInfo(gridpane);
+		SwitchSceneUtil.turnToCusController_CusInfo();
 	}
 	
 	@FXML
 	void turnToCusController_HotelList() {
-		SwitchSceneUtil.turnToCusController_HotelList(gridpane);
+		SwitchSceneUtil.turnToCusController_HotelList();
 	}
 	
 	@FXML
 	void turnToCusController_HistoryOrderList() {
-		SwitchSceneUtil.turnToCusController_HistoryOrderList(gridpane);
+		SwitchSceneUtil.turnToCusController_HistoryOrderList();
 	}
 	
 	@FXML
 	void turnToCusController_UnexcutedOrderList() {
-		SwitchSceneUtil.turnToCusController_UnexcutedOrderList(gridpane);
+		SwitchSceneUtil.turnToCusController_UnexcutedOrderList();
 	}
 }
--- HotelBookingSystem/client/src/main/java/org/client/presentation/customer/SwitchSceneUtil.java
+++ HotelBookingSystem/client/src/main/java/org/client/presentation/customer/SwitchSceneUtil.java
@@ -18,58 +18,58 @@ public class SwitchSceneUtil {
 		SwitchSceneUtil.stage = stage;
 	}
 	
-	static void turnToCusController_Main(GridPane gridpane) {
-		switchindex(gridpane, "/客户/导航_主界面.fxml");
+	static void turnToCusController_Main() {
+		switchindex("/客户/导航_主界面.fxml");
 		stage.setTitle("酒店预订管理系统");
 		stage.show();
 	}
 	
-	static void turnToCusController_CusInfo(GridPane gridpane) {
-		switchindex(gridpane, "/客户/导航_客户信息.fxml");
+	static void turnToCusController_CusInfo() {
+		switchindex("/客户/导航_客户信息.fxml");
 		turnToCustomerInfo(new AnchorPane());
 	}
 	
-	static void turnToCusController_HotelList(GridPane gridpane) {
-		switchindex(gridpane, "/客户/导航_酒店列表.fxml");
+	static void turnToCusController_HotelList() {
+		switchindex("/客户/导航_酒店列表.fxml");
 		turnToCustomerHotelList(new AnchorPane());
 	}
 	
-	static void turnToCusController_HistoryOrderList(GridPane gridpane) {
-		switchindex(gridpane, "/客户/导航_历史订单.fxml");
+	static void turnToCusController_HistoryOrderList() {
+		switchindex("/客户/导航_历史订单.fxml");
 		turnToCustomerHistoryOrderList(new AnchorPane());
 	}
 	
-	static void turnToCusController_UnexcutedOrderList(GridPane gridpane) {
-		switchindex(gridpane, "/客户/导航_未执行订单.fxml");
+	static void turnToCusController_UnexcutedOrderList() {
+		switchindex("/客户/导航_未执行订单.fxml");
 		turnToCustomerUnexcutedOrderList(new AnchorPane());
 	}
 	
 	static void turnToCustomerInfo(AnchorPane root) {
-		turnToAnotherScene(gridpane, root, "/客户/查看客户信息界面.fxml");
+		turnToAnotherScene(root, "/客户/查看客户信息界面.fxml");
 	}
 	
 	static void turnToCustomerHotelList(AnchorPane root) {
-		turnToAnotherScene(gridpane, root, "/客户/浏览酒店界面.fxml");
+		turnToAnotherScene(root, "/客户/浏览酒店界面.fxml");
 	}
 	
 	static void turnToCustomerHistoryOrderList(AnchorPane root) {
-		turnToAnotherScene(gridpane, root, "/客户/浏览客户历史订单界面.fxml");
+		turnToAnotherScene(root, "/客户/浏览客户历史订单界面.fxml");
 	}
 	
 	static void turnToCustomerUnexcutedOrderList(AnchorPane root) {
-		turnToAnotherScene(gridpane, root, "/客户/浏览客户未执行订单界面.fxml");
+		turnToAnotherScene(root, "/客户/浏览客户未执行订单界面.fxml");
 	}
 	
 	static void turnToCustomerCheckCredit(AnchorPane root) {
-		turnToAnotherScene(gridpane, root, "/客户/查看信用记录界面.fxml");
+		turnToAnotherScene(root, "/客户/查看信用记录界面.fxml");
 	}
 	
 	static void turnToCustomerModifyInfo(AnchorPane root) {
-		turnToAnotherScene(gridpane, root, "/客户/修改客户信息界面.fxml");
+		turnToAnotherScene(root, "/客户/修改客户信息界面.fxml");
 	}
 	
 	static void turnToCustomerGenerateOrder(AnchorPane root) {
-		turnToAnotherScene(gridpane, root, "/客户/生成订单界面.fxml");
+		turnToAnotherScene(root, "/客户/生成订单界面.fxml");
 	}
 	
 	
@@ -77,11 +77,11 @@ public class SwitchSceneUtil {
 	/*
 	 * 此方法用于加载导航栏
 	 */
-	private static void switchindex(GridPane gridpane, String resource) {
-		SwitchSceneUtil.gridpane = gridpane;
+	private static void switchindex(String resource) {
 		try {
-			Parent root = FXMLLoader.load(SwitchSceneUtil.class.getResource(resource));
-			stage.setScene(new Scene(root));
+			Parent parent = FXMLLoader.load(SwitchSceneUtil.class.getResource(resource));
+			stage.setScene(new Scene(parent));
+			gridpane = getGridpane(parent);
 		} catch (IOException ex) {
 			ex.printStackTrace();
 		}
@@ -90,13 +90,21 @@ public class SwitchSceneUtil {
 	/*
 	 * 此方法用于加载除了导航栏以外的界面
 	 */
-	private static void turnToAnotherScene(GridPane gridpane, AnchorPane root, String resource) {
+	private static void turnToAnotherScene(AnchorPane root, String resource) {
 		try {
 			root = FXMLLoader.load(SwitchSceneUtil.class.getResource(resource));
 		} catch (IOException ex) {
 			ex.printStackTrace();
 		}
 		GridPane.setConstraints(root, 1, 0);
-		gridpane.getChildren().set(1, root);
+		if (gridpane.getChildren().size() > 1) {
+			gridpane.getChildren().set(1, root);
+		} else {
+			gridpane.getChildren().add(1, root);
+		}
+	}
+	
+	private static GridPane getGridpane(Parent parent) {
+		return (GridPane)(((Parent)((Parent)parent.getChildrenUnmodifiable().get(1)).getChildrenUnmodifiable().get(0)).getChildrenUnmodifiable().get(0));
 	}
 }
