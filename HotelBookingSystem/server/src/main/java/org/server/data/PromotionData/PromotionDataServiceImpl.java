a74097107b4aa5ec5a8897e75687aaff9e1ba4e7
fraliphsoft <151250054@smail.nju.edu.cn>
2016-11-29 19:57:39 +0800

    修改了沟通数据库的接口方法

--- HotelBookingSystem/server/src/main/java/org/server/data/PromotionData/PromotionDataServiceImpl.java
+++ HotelBookingSystem/server/src/main/java/org/server/data/PromotionData/PromotionDataServiceImpl.java
@@ -47,16 +47,16 @@ public class PromotionDataServiceImpl extends UnicastRemoteObject implements Pro
 	}
 
 	public LevelPO showLevel() throws RemoteException {
-		communicator = DatabaseCommunicator.getInstance();
-		ArrayList<Integer> levelNumList = new ArrayList<>(communicator.getLevel_LevelNum());
-		ArrayList<Double> creditsList = new ArrayList<>(communicator.getLevel_Credits());
-		ArrayList<Double> badList = new ArrayList<>();
-		for (double sadNum:creditsList) {
-			badList.add(sadNum);
-		}
-		LevelPO levelPO = new LevelPO(levelNumList.size(), badList);
+//		communicator = DatabaseCommunicator.getInstance();
+//		ArrayList<Integer> levelNumList = new ArrayList<>(communicator.getLevel_LevelNum());
+//		ArrayList<Double> creditsList = new ArrayList<>(communicator.getLevel_Credits());
+//		ArrayList<Double> badList = new ArrayList<>();
+//		for (double sadNum:creditsList) {
+//			badList.add(sadNum);
+//		}
+//		LevelPO levelPO = new LevelPO(levelNumList.size(), badList);
 //		LevelPO levelPO = new LevelPO(levelNumList.size(), creditsList);
-		return levelPO;
+		return null;
 	}
 
 	public ResultMessage modifyLevel(LevelPO po) throws RemoteException {
