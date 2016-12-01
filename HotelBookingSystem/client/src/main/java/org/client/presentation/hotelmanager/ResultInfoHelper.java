e73026ec0db36b39c11bb52350fa63284d2e8422
foxwel88 <cttony1997@126.com>
2016-12-01 19:31:37 +0800

    修改格式

--- HotelBookingSystem/client/src/main/java/org/client/presentation/hotelmanager/ResultInfoHelper.java
+++ HotelBookingSystem/client/src/main/java/org/client/presentation/hotelmanager/ResultInfoHelper.java
@@ -11,17 +11,17 @@ import org.common.utility.ResultMessage;
 public class ResultInfoHelper {
 
     /**将提示信息显示在一个Label上 */
-    public static void setResultLabel(Label label, ResultMessage resultMessage) {
-        switch(resultMessage) {
-            case SUCCESS:
-                label.setText("编辑成功");
-                break;
-            case WRONG_FORMAT:
-                label.setText("输入信息格式有误");
-                break;
-            default:
-                label.setText("错误");
-                break;
-        }
-    }
+	public static void setResultLabel(Label label, ResultMessage resultMessage) {
+		switch(resultMessage) {
+			case SUCCESS:
+				label.setText("编辑成功");
+				break;
+			case WRONG_FORMAT:
+				label.setText("输入信息格式有误");
+				break;
+			default:
+				label.setText("错误");
+				break;
+		}
+	}
 }
