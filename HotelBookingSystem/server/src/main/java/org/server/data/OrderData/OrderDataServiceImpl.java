6408f2a90f1dd454953dc1f9903bce76eaf49ed6
foxwel88 <cttony1997@126.com>
2016-12-02 19:17:59 +0800

    Order数据层

--- HotelBookingSystem/server/src/main/java/org/server/data/OrderData/OrderDataServiceImpl.java
+++ HotelBookingSystem/server/src/main/java/org/server/data/OrderData/OrderDataServiceImpl.java
@@ -2,12 +2,21 @@ package org.server.data.OrderData;
 
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import org.common.dataservice.OrderDataService.OrderDataService;
+import org.common.po.LevelPO;
 import org.common.po.OrderPO;
 import org.common.utility.OrderType;
 import org.common.utility.ResultMessage;
+import org.common.utility.RoomType;
+
+import mySQL.DatabaseCommunicator;
 
 public class OrderDataServiceImpl extends UnicastRemoteObject implements OrderDataService {
 
@@ -20,25 +29,109 @@ public class OrderDataServiceImpl extends UnicastRemoteObject implements OrderDa
 		System.out.println("order start");
 		// TODO Auto-generated constructor stub
 	}
+	
+	OrderPO getPOfromSet(ResultSet resultSet) {
+		OrderPO po = null;
+		try {
+			OrderType type = OrderType.getType((resultSet.getString("type")));
+			String userId = resultSet.getString("userId");
+			Date generatedDate = resultSet.getDate("generatedDate");
+			Date schFrom = resultSet.getDate("schFrom");
+			Date schTo = resultSet.getDate("schTo");
+			Date actFrom = resultSet.getDate("actFrom");
+			Date actTo = resultSet.getDate("actTo");
+			Date latestTime = resultSet.getDate("latestTime");
+			Date cancelTime = resultSet.getDate("cancelTime");
+			String hotelAddress = resultSet.getString("hotelAddress");
+			String hotelName = resultSet.getString("hotelName");
+			RoomType roomType = RoomType.getType(resultSet.getString("roomType"));
+			String orderID = resultSet.getString("orderID");
+			double totalPrice = resultSet.getDouble("totalPrice");
+			int roomNum = resultSet.getInt("totalPrice");
+			int numOfPeople = resultSet.getInt("numOfPeople");
+			boolean existsChild = resultSet.getBoolean("existsChild");
+			String customerName = resultSet.getString("customerName");
+			String phoneNumber = resultSet.getString("phoneNumber");
+			po = new OrderPO(type,generatedDate,schFrom,schTo,actFrom,actTo,latestTime,cancelTime,hotelAddress,orderID,hotelName,
+					roomType,totalPrice,roomNum,numOfPeople,existsChild,customerName,userId,phoneNumber);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return po;
+	}
 
 	public OrderPO getOrderPO(String orderID) throws RemoteException {
-		// TODO Auto-generated method stub
-		return null;
+		OrderPO po = null;
+		PreparedStatement preparedStatement;
+		try {
+			preparedStatement = DatabaseCommunicator.getConnectionInstance().prepareStatement("select type,userId,generatedDate,"
+					+ "schFrom,schTo,actFrom,actTo,latestTime,cancelTime,hotelAddress,orderID,hotelName,roomType,totalPrice,roomNum,numOfPeople,"
+					+ "existsChild,customerName,phoneNumber from `Order` where orderID=" + orderID);
+			
+			ResultSet resultSet = DatabaseCommunicator.execute(preparedStatement);
+			while (resultSet.next()) {
+				po = getPOfromSet(resultSet);
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return po;
 	}
 
 	public List<OrderPO> getUserOrderPO(String userID, OrderType type) throws RemoteException {
-		// TODO Auto-generated method stub
-		return null;
+		List<OrderPO> res = new ArrayList<OrderPO>();
+		PreparedStatement preparedStatement;
+		try {
+			preparedStatement = DatabaseCommunicator.getConnectionInstance().prepareStatement("select type,userId,generatedDate,"
+					+ "schFrom,schTo,actFrom,actTo,latestTime,cancelTime,hotelAddress,orderID,hotelName,roomType,totalPrice,roomNum,numOfPeople,"
+					+ "existsChild,customerName,phoneNumber from `Order` where userId=" + userID + " and type=" + type.getString());
+			ResultSet resultSet = DatabaseCommunicator.execute(preparedStatement);
+			while (resultSet.next()) {
+				res.add(getPOfromSet(resultSet));
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return res;
 	}
 
 	public List<OrderPO> getHotelOrderPO(String hotelID, OrderType type) throws RemoteException {
-		// TODO Auto-generated method stub
-		return null;
+		List<OrderPO> res = new ArrayList<OrderPO>();
+		PreparedStatement preparedStatement;
+		try {
+			preparedStatement = DatabaseCommunicator.getConnectionInstance().prepareStatement("select type,userId,generatedDate,"
+					+ "schFrom,schTo,actFrom,actTo,latestTime,cancelTime,hotelAddress,orderID,hotelName,roomType,totalPrice,roomNum,numOfPeople,"
+					+ "existsChild,customerName,phoneNumber from `Order` where hotelAddress=" + hotelID + " and type=" + type.getString());
+			ResultSet resultSet = DatabaseCommunicator.execute(preparedStatement);
+			while (resultSet.next()) {
+				res.add(getPOfromSet(resultSet));
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return res;
 	}
 
 	public List<OrderPO> getAbnormalOrderPO() throws RemoteException {
-		// TODO Auto-generated method stub
-		return null;
+		List<OrderPO> res = new ArrayList<OrderPO>();
+		PreparedStatement preparedStatement;
+		try {
+			preparedStatement = DatabaseCommunicator.getConnectionInstance().prepareStatement("select type,userId,generatedDate,"
+					+ "schFrom,schTo,actFrom,actTo,latestTime,cancelTime,hotelAddress,orderID,hotelName,roomType,totalPrice,roomNum,numOfPeople,"
+					+ "existsChild,customerName,phoneNumber from `Order` where type=" + OrderType.ABNORMAL.getString());
+			ResultSet resultSet = DatabaseCommunicator.execute(preparedStatement);
+			while (resultSet.next()) {
+				res.add(getPOfromSet(resultSet));
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return res;
 	}
 
 	public ResultMessage add(OrderPO po) throws RemoteException {
@@ -60,6 +153,11 @@ public class OrderDataServiceImpl extends UnicastRemoteObject implements OrderDa
 		// TODO Auto-generated method stub
 		
 	}
-
+	
+	public static void main(String[] args) throws RemoteException {
+		OrderDataServiceImpl mywork = new OrderDataServiceImpl();
+		DatabaseCommunicator.databaseInit();
+		mywork.getOrderPO("1");
+	}
 	
 }
