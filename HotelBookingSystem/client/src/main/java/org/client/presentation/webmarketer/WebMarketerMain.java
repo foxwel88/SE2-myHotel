edca17f11a9260164927b2682c74060700e67902
Gyue15 <786330297@qq.com>
2016-12-05 22:37:33 +0800

    更改网站营销人员主界面

--- HotelBookingSystem/client/src/main/java/org/client/presentation/webmarketer/WebMarketerMain.java
+++ HotelBookingSystem/client/src/main/java/org/client/presentation/webmarketer/WebMarketerMain.java
@@ -1,9 +1,17 @@
 package org.client.presentation.webmarketer;
 
 import java.net.URL;
+import java.rmi.RemoteException;
 import java.util.ResourceBundle;
 
+import org.client.rmi.RMIHelper;
+
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.util.Duration;
 
 /**
  * 
@@ -20,9 +28,23 @@ public class WebMarketerMain {
 
 	@FXML
     private URL location;
+	
+	@FXML
+	private Label timeLabel;
 
 	@FXML
     void initialize() {
+		assert timeLabel != null : "fx:id=\"timeLabel\" was not injected: check your FXML file '网站营销人员主界面.fxml'.";
+		//clock
+		Timeline timeline = new Timeline(new KeyFrame(Duration.seconds(0), actionEvent -> {
+			try {
+				timeLabel.setText("当前时间：" + RMIHelper.getInstance().getTimeServiceImpl().getCurrentTime());
+			} catch (RemoteException e) {
+				e.printStackTrace();
+			}
+		}), new KeyFrame(Duration.seconds(1)));
+		timeline.setCycleCount(Animation.INDEFINITE);
+		timeline.play();
 	}
 	
 }
